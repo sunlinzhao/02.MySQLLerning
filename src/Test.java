ecaa972e739310020e0dc3028aec569be8f66236
sunlinzhao <2436949535@qq.com>
2024-08-06 20:05:28 +0800

    Exclude out directory from Git

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,5 @@
-/**
- * @ Author : SunLZ
- * @ Project : MySQLLerning
- * @ Date : 2024/8/2
- */
+import java.util.*;
+
 public class Test {
-    public static void main(String[] args) {
 
-
-    }
 }
